b30e43beff653026b7ab16b26763b12ddbcd5a93
Raulanthropos <ipsichias@yandex.com>
2023-04-11 23:18:15 +0300

    added redux-persist

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -14,6 +14,7 @@ const CompanySearchResults = () => {
 
   useEffect(() => {
     getJobs()
+    //eslint-disable-next-line
   }, [])
 
   const getJobs = async () => {
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,19 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit'
-import mainReducer from '../reducers'
-import jobReducer from '../reducers/job'
-import { combineReducers } from '@reduxjs/toolkit'
+import mainReducer from "../reducers";
+import jobReducer from "../reducers/job";
+import localStorage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 // configureStore will set up the Redux Store for us!
 
-const bigReducer = combineReducers ({
+const bigReducer = combineReducers({
   favourite: mainReducer,
-  job: jobReducer
-})
+  job: jobReducer,
+});
 
-const store = configureStore({
-  reducer: bigReducer,
-})
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+  whitelist: ["favourite", "job"],
+};
 
-export default store
+const persistedReducer = persistReducer(persistConfig, bigReducer);
 
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
 // now the store is ready! let's INJECT IT into our REACT APP!
 // we do it in the src/index.js file
